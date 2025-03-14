9ecb703e7c7d1ccc90a5d661020a6fe9b1d917b8
Samuele Verzi <samu@stacklok.com>
2025-03-14 17:52:23 +0100

    feat(markdown): upgrade libs and render markdown for fim and chat

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const getMarkdownNormalize = (isFim: boolean, code: string) => `${isFim ? '```python ' : ''}
+${code}
+${isFim ? '```' : ''}`
