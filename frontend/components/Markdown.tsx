beb69e31697c5b71166863435ef7dfa6e224bb45
Samuele V <4377202+samuv@users.noreply.github.com>
2025-03-17 14:48:47 +0100

    feat(markdown): upgrade libs and render markdown for fim and chat (#47)

---
+++ frontend/components/Markdown.tsx
@@ -0,0 +1,15 @@
+
+import { MarkdownHooks } from 'react-markdown'
+import rehypeStarryNight from 'rehype-starry-night'
+import remarkGfm from 'remark-gfm'
+
+export const Markdown = ({ value }: {value: string}) => {
+  console.log(JSON.stringify(value))
+  return (
+    <div className="markdown-body ov">
+      <MarkdownHooks remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeStarryNight]}>
+        {value}
+      </MarkdownHooks>
+    </div>
+  )
+}
